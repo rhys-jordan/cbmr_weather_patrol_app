6cccfa8620c35e8833bedd00e01cabdd26ce61e5
Nate Rengo <naterengo@gmail.com>
2025-04-09 11:41:35 -0600

    IT works!! FIxed pm form

--- CBMR_Weather/routes/pm_form.py
+++ CBMR_Weather/routes/pm_form.py
@@ -16,26 +16,25 @@ def pm_form():
         hs = request.form.get('hs', None)
         hn24 = request.form.get('hn24', None)
         ytd_snow = request.form.get('ytd_snow', None)
+        ytd_swe = request.form.get('ytd_swe', None)
         weather_fx = request.form.get('weather_fx', None)
-        tonight = request.form.get('tonight', None)
-        tomorrow = request.form.get('tomorrow', None)
-        tomorrow_night = request.form.get('tomorrow_night', None)
+        tonight_tomorrow = request.form.get('tonight_tomorrow', None)
+        do_today = request.form.get('do_today', None)
+        plan_to_do = request.form.get('plan_to_do', None)
         mitigation = request.form.get('mitigation', None)
-
         if request.form.get('uphill_access', None) == 'paradise':
-            uphill_access =  'Open to top of Paradise'
+            uphill_access = 'Open to top of Paradise'
         elif request.form.get('uphill_access', None) == 'peanut':
             uphill_access = 'Open to Peanut'
         else:
             uphill_access = 'Not Open'
-        basic_stats = [hs, hn24, ytd_snow, uphill_access]
-
-        pdf_filename = generate_pdf_pm(date, forecaster, basic_stats, weather_fx, tonight, tomorrow, tomorrow_night, mitigation)
-        return send_file(pdf_filename, as_attachment=True)  #
-
+        basic_stats = [hs, hn24, ytd_snow, ytd_swe, uphill_access]
+        pdf_filename = generate_pdf_pm(date, forecaster, basic_stats, weather_fx, tonight_tomorrow, do_today,
+                                       plan_to_do, mitigation)
+        return send_file(pdf_filename, as_attachment=True)
 
     else:
         now = datetime.now()
         formatted_now = now.strftime("%Y-%m-%dT%H:%M")
         snow = Snow.query.filter_by(date=now.strftime("%Y-%m-%d")).first()
-        return render_template('pm-form.html', now=formatted_now, snow=snow) #, oldSnow=snow,avalanches=avalanche)
+        return render_template('pm-form.html', now=formatted_now, snow=snow)  # , oldSnow=snow,avalanches=avalanche)
