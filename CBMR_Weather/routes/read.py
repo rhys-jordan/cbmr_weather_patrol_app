0cf9394c2b2845b1418ab756667a67b3b6f9164b
rhys-jordan <rhysjordan26@gmail.com>
2025-04-20 16:51:12 -0600

    New graphs made - YTD Snow - YTD Swe - Temperature

--- CBMR_Weather/routes/read.py
+++ CBMR_Weather/routes/read.py
@@ -30,18 +30,72 @@ def get_swe_snow():
     swe = []
     for row in ytd_snow:
         date_format = datetime.strftime(row[1], '%m-%d-%Y')
-
         if (row[0] != None):
             dates.append(date_format)
             # dates.append(str(inct))
             swe.append(row[0])
     return dates, swe
 
+def get_hn24_ytd_snow(start_date, end_date):
+    hn24_snow = db.session.query(Snow.hn24, Snow.date).filter(Snow.date.between(start_date,end_date)).order_by(Snow.date)
+    dates = []
+    ytd_snow = []
+    past_hn24 = 0
+    for row in hn24_snow:
+        date_format = datetime.strftime(row[1], '%m-%d-%Y')
+        #day = datetime.strftime(row[1], '%d')
+        #month = datetime.strftime(row[2], '%m')
+        dates.append(date_format)
+        #dates.append(str(row[2]) + '/' + str(row[1]))
+        if (row[0] != None and type(row[0]) != str):
+            #ytd_snow.append(row[0])
+            ytd_snow.append(row[0] + past_hn24)
+            past_hn24 = row[0] + past_hn24
+        else:
+            ytd_snow.append(0 + past_hn24 )
+    return dates, ytd_snow
+
+
+def get_swe_ytd_swe(start_date, end_date):
+    swe = db.session.query(Snow.swe, Snow.date).filter(Snow.date.between(start_date,end_date)).order_by(Snow.date)
+    dates = []
+    ytd_swe = []
+    past_hn24 = 0
+    for row in swe:
+        date_format = datetime.strftime(row[1], '%m-%d-%Y')
+        dates.append(date_format)
+        if (row[0] != None and type(row[0]) != str):
+            ytd_swe.append(row[0] + past_hn24)
+            past_hn24 = row[0] + past_hn24
+        else:
+            ytd_swe.append(0 + past_hn24 )
+    return dates, ytd_swe
+
+def get_temp(start_date, end_date):
+    temps = db.session.query(Snow.temperature, Snow.date).filter(Snow.date.between(start_date,end_date)).order_by(Snow.date)
+    dates = []
+    temperatures = []
+    for row in temps:
+        date_format = datetime.strftime(row[1], '%m-%d-%Y')
+        if (row[0] != None):
+            dates.append(date_format)
+            # dates.append(str(inct))
+            temperatures.append(row[0])
+    return dates, temperatures
+
 
 @bp_read.route("/read", methods=['GET', 'POST'])
 def read():
     dates_snow, snow = get_ytd_snow()
     dates_swe, swe = get_swe_snow()
+    dates_ytd, hn24_ytd = get_hn24_ytd_snow('2024-10-01', '2025-05-01')
+    dates_ytd_2, hn24_2 = get_hn24_ytd_snow('2024-10-01', '2025-05-01')
+    dates_ytd_swe, ytd_swe = get_swe_ytd_swe('2024-10-01', '2025-05-01')
+    #really struggling with comparing two seasons ytd snow
+    #got to get the dates to line up correctly
 
-    return render_template('read.html', dates_snow = json.dumps(dates_snow), ytd_snow = snow,
-                            dates_swe = json.dumps(dates_swe), ytd_swe = swe)
+    dates_temp, temperatures = get_temp('2024-10-01', '2025-05-01')
+
+    return render_template('read.html', dates_snow = json.dumps(dates_ytd), ytd_snow = hn24_ytd,
+                            dates_swe = json.dumps(dates_ytd_swe), ytd_swe = ytd_swe,
+                           dates_temp =json.dumps(dates_temp), temps = temperatures)
--- CBMR_Weather/static/graphs.js
+++ CBMR_Weather/static/graphs.js
@@ -1,8 +1,44 @@
 
 function graphs(x, y, element_id){
+    const graphData = {
+      labels: x[0],
+      datasets: []
+    };
 
+    for(let i = 0; i < x.length; i++){
+        const dataset = {
+          label: `Dataset ${i + 1}`,
+          data: y[i],
+          borderColor: `hsl(${i * 120}, 100%, 50%)`,
+          fill: false
+        };
+        graphData.datasets.push(dataset);
+    }
+
+    const xValues = x[0];
+    const yValues = y[0];
+    console.log(xValues)
+    console.log(yValues)
+    const y_max = Math.max(...yValues)
+    const y_min = Math.min(...yValues)
+
+    new Chart(document.getElementById(element_id).getContext('2d'), {
+      type: "line",
+      data: graphData,
+      options: {
+        legend: {display: true},
+        scales: {
+          yAxes: [{ticks: {min:y_min, max:y_max + 10}}]
+        }
+      }
+    });
+};
+
+function graphs2(x, y, element_id){
     const xValues = x;
     const yValues = y;
+//    console.log(xValues)
+//    console.log(yValues)
     const y_max = Math.max(...yValues)
     const y_min = Math.min(...yValues)
 
@@ -19,36 +55,79 @@ function graphs(x, y, element_id){
         }]
       },
       options: {
+        title: {
+          display: true,
+          text: 'YTD Snow',
+          padding: 20,
+          fontSize: 18
+        },
         legend: {display: false},
         scales: {
-          yAxes: [{ticks: {min:y_min, max:y_max}}]
+          xAxes: [{
+            scaleLabel: {
+              display: true,
+              labelString: 'Date',
+              fontSize: 14
+            }
+          }],
+          yAxes: [{
+            scaleLabel: {
+              display: true,
+              labelString: 'YTD Snow (inches)',
+              fontSize: 14
+            }
+          }]
         }
       }
     });
 };
 
 
-//(async function() {
-//    const xValues = JSON.parse({{dates|tojson}});
-//    const yValues = JSON.parse("{{ytd_snow}}");
-//
-//    snow_graph = new Chart( document.getElementById("ytd_snow_graph"), {
-//      type: "line",
-//      data: {
-//        labels: xValues,
-//        datasets: [{
-//          fill: false,
-//          lineTension: 0,
-//          backgroundColor: "rgba(0,0,255,1.0)",
-//          borderColor: "rgba(0,0,255,0.1)",
-//          data: yValues
-//        }]
-//      },
-//      options: {
-//        legend: {display: false},
-//        scales: {
-//          yAxes: [{ticks: {min: 20, max:120}}]
-//        }
-//      }
-//    })();
+function graphs3(x, y, element_id, chart_title, x_label, y_label){
+    const xValues = x;
+    const yValues = y;
+//    console.log(xValues)
+//    console.log(yValues)
+    const y_max = Math.max(...yValues)
+    const y_min = Math.min(...yValues)
+
+    new Chart(document.getElementById(element_id).getContext('2d'), {
+      type: "line",
+      data: {
+        labels: xValues,
+        datasets: [{
+          fill: false,
+          lineTension: 0,
+          backgroundColor: "rgba(0,0,255,1.0)",
+          borderColor: "rgba(0,0,255,0.1)",
+          data: yValues
+        }]
+      },
+      options: {
+        title: {
+          display: true,
+          text: chart_title,
+          padding: 20,
+          fontSize: 18
+        },
+        legend: {display: false},
+        scales: {
+          xAxes: [{
+            scaleLabel: {
+              display: true,
+              labelString: x_label,
+              fontSize: 14
+            }
+          }],
+          yAxes: [{
+            scaleLabel: {
+              display: true,
+              labelString: y_label,
+              fontSize: 14
+            }
+          }]
+        }
+      }
+    });
+};
 
